d80e0d80ed68226d9850dd91a9181521b24a608f
Aaron Martin <aaron.martin@shopgate.com>
2018-02-23 16:00:09 -0600

    WEBC-633: modified shopify.api.js to use shopify-api-node

--- extension/lib/shopify.api.js
+++ extension/lib/shopify.api.js
@@ -1,114 +1,37 @@
-const fetch = require('node-fetch')
-const InvalidResponseError = require('../models/errors/InvalidResponseError')
+const ShopifyClient = require('shopify-api-node')
+const STOREFRONT_ACCESS_TOKEN_TITLE = 'shopgate-app-access-token'
 
 /**
  * Class for interacting with the Shopify-APIs
  */
-class Shopify {
+class ShopifyWrapper {
   /**
    * @typedef {object} ShopifyConfig
    * @property {string} shopifyShopAlias
    * @property {string} shopifyAccessToken
    * @param {ShopifyConfig} ShopifyConfig
    */
-  constructor (ShopifyConfig) {
-    this.config = ShopifyConfig
-    this.shopDomain = `https://${ShopifyConfig.shopifyShopAlias}.myshopify.com`
-    this.targetTokenTitle = 'Access Token for WEBC-546' // TODO Has to be part of the config
+  constructor (shop, accessToken) {
+    this._shopifyAdminClient = new ShopifyClient({shopName: shop, accessToken: accessToken})
   }
 
   /**
-   * Returns the product count for collection endpoint
-   * @param {string} collectionId
-   * @returns {string}
+   * @param {string} id
+   * @return {Promise.<void>}
    */
-  getCollectionProductCountUrl (collectionId) {
-    return this.shopDomain + '/admin/products/count.json?collection_id=' + collectionId
+  async getProductCountByCollectionId (id) {
+    const products = await this._shopifyAdminClient.product.list({collection_id: id})
+    console.log(products)
+    return products.length
   }
 
   /**
-   * Returns the GraphQL endpoint
-   * @returns {string}
+   * @return {Promise.<*>}
    */
-  getGraphQlUrl () {
-    return this.shopDomain + '/api/graphql'
-  }
-
-  /**
-   * Returns the storefront access token endpoint
-   * @returns {string}
-   */
-  getStorefrontAccessTokenUrl () {
-    return this.shopDomain + '/admin/storefront_access_tokens.json'
-  }
-
-  /**
-   * Returns the request header for sending requests to the Admin-API
-   * @returns {object}
-   */
-  getAdminApiRequestHeader () {
-    return {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': this.config.shopifyAccessToken
-      }
-    }
-  }
-
-  /**
-   * Returns the request header for sending requests to the GraphQL-API, also get's the necessary storefront access token
-   * @param {string} body
-   * @returns {Promise.<{object}>}
-   */
-  async getGraphQlApiRequestHeader (body) {
-    return {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Shopify-Storefront-Access-Token': await this.getStorefrontAccessToken()
-      },
-      body: body
-    }
-  }
-
-  /**
-   * Returns the storefront access token, which is requested via Admin-API
-   * @returns {Promise.<{string}>}
-   */
-  async getStorefrontAccessToken () {
-    const response = await fetch(this.getStorefrontAccessTokenUrl(), this.getAdminApiRequestHeader())
-
-    let json = null
-
-    try {
-      /**
-       * @typedef {object} json
-       * @property {array} storefront_access_tokens
-       */
-      json = await response.json()
-    } catch (err) {
-      throw new InvalidResponseError(err.message)
-    }
-
-    if (!json.storefront_access_tokens) {
-      throw new InvalidResponseError('Can\'t find json.storefront_access_tokens in response from Admin-API')
-    }
-
-    let token = null
-    // Iterate through the tokens, we only need that one that's equal to the targetTokenTitle
-    for (const storefrontAccessToken of json.storefront_access_tokens) {
-      if (storefrontAccessToken.title === this.targetTokenTitle) {
-        token = storefrontAccessToken.access_token
-      }
-    }
-
-    if (!token) {
-      throw new InvalidResponseError('Can\'t find matching Storefront-Accesstoken for \'' + this.targetTokenTitle + '\'')
-    }
-
+  async getStorefrontAccessTokenNew () {
+    const token = await this._shopifyAdminClient.storefrontAccessToken.create({title: STOREFRONT_ACCESS_TOKEN_TITLE})
     return token
   }
 }
 
-module.exports = Shopify
+module.exports = ShopifyWrapper
