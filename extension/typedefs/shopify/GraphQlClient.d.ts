47fb63e76537666975da3ebf7bde620afe465a91
Oliver Müller <oliver.mueller@shopgate.com>
2018-02-27 09:55:44 +0100

    WEBC-631: Fixed typo in type definition file GraphQlClient.d.ts

--- extension/typedefs/shopify/GraphQlClient.d.ts
+++ extension/typedefs/shopify/GraphQlClient.d.ts
@@ -3,7 +3,7 @@ interface GraphQlClient {
 
 	fetchNextPage(nodeOrNodes: GraphModel | GraphModel[], options: Object): Promise<GraphModel[]>;
 
-	send(request: Query | Mutation | Document | Functionm, variableValues?: Object, otherProperties?: Object): Promise<Object>;
+	send(request: Query | Mutation | Document | Function, variableValues?: Object, otherProperties?: Object): Promise<Object>;
 
 	query(name?: string, variables?: VariableDefinition[], selectionSetCallback?: Function): Query;
 
