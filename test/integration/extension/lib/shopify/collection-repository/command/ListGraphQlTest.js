d9e9e8e20c36a5ea69c2d8601d74ce4a05a66386
Aaron Martin <aaron.martin@shopgate.com>
2018-03-02 15:28:28 -0600

    WEBC-626: added an integartion test to a paginated request to the graphQl client

--- test/integration/extension/lib/shopify/collection-repository/command/ListGraphQlTest.js
+++ test/integration/extension/lib/shopify/collection-repository/command/ListGraphQlTest.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 const StoreFrontApiFactory = require('../../../../../../../extension/lib/shopify/StorefrontClientFactory')
 const ListGraphQl = require('../../../../../../../extension/lib/shopify/collection-repository/command/ListGraphQl')
 const integrationCredentials = require('../../../../../../../.integration-credentials')
+const sinon = require('sinon')
 
 describe('ListGraphQl Command', () => {
   it('should return shopify categories', async () => {
@@ -19,5 +20,22 @@ describe('ListGraphQl Command', () => {
 
     assert.equal('Integration-Test *Do no touch*', testCategory.title)
     assert.equal('https://cdn.shopify.com/s/files/1/0005/5731/8207/collections/Download.png?v=1519823424', testCategory.image)
+  })
+
+  it('should make a paginated request', async () => {
+    const storeFrontApiFactory = new StoreFrontApiFactory(integrationCredentials.storefrontAccessToken, integrationCredentials.shopName)
+    const shopifyStorefrontClient = storeFrontApiFactory.create()
+    const listCommandControl = new ListGraphQl(shopifyStorefrontClient)
+    const listCommand = new ListGraphQl(shopifyStorefrontClient)
+    listCommand.numberPerPage = 1
+    const makePaginatedRequestExperimentalSpy = sinon.spy(listCommand, '_makePaginatedRequests')
+    const makePaginatedRequestControlSpy = sinon.spy(listCommandControl, '_makePaginatedRequests')
+    const listCommandControlGroup = listCommandControl.execute()
+    const listCommandExperimentalGroup = listCommand.execute()
+    const [controlGroup, experimentalGroup] = await Promise.all([listCommandControlGroup, listCommandExperimentalGroup])
+
+    assert.equal(makePaginatedRequestExperimentalSpy.callCount, controlGroup.length)
+    assert.equal(makePaginatedRequestControlSpy.callCount, 1)
+    assert.equal(experimentalGroup.length, controlGroup.length)
   })
 })
