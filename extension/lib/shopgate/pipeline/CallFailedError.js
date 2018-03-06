fd98976b391ca93854d00f480a5a2a7f5de7fdac
Sofija <sofija.blazevski@shopgate.com>
2018-03-06 14:02:28 +0100

    WEBC-632-storefront-authorisation Added typehints

--- extension/lib/shopgate/pipeline/CallFailedError.js
+++ extension/lib/shopgate/pipeline/CallFailedError.js
@@ -1,9 +1,15 @@
 class ShopgatePipelineCallFailedError extends Error {
+  /**
+   * @param {Error} previous
+   */
   constructor (previous) {
     super('Failed processing pipeline request.')
     this._previous = previous
   }
 
+  /**
+   * @returns {Error}
+   */
   get previous () {
     return this._previous
   }
