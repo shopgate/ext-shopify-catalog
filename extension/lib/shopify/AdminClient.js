c771e48573219b5bc58221c21680f9ca9873ce95
Sofija <sofija.blazevski@shopgate.com>
2018-03-06 14:07:40 +0100

    WEBC-632-storefront-authorisation Removed unused method.

--- extension/lib/shopify/AdminClient.js
+++ extension/lib/shopify/AdminClient.js
@@ -16,26 +16,6 @@ class ShopifyAdminClient extends ShopifyClient {
   async createStorefrontAccessToken () {
     return this.storefrontAccessToken.create({title: STOREFRONT_ACCESS_TOKEN_TITLE})
   }
-
-  /**
-   * @return {Promise<string>}
-   */
-  async getFirstApplicableStorefrontAccessToken () {
-    const accessTokens = await this.getStorefrontAccessTokens()
-    const applicableAccessToken = accessTokens.find(token => {
-      if (token.title === STOREFRONT_ACCESS_TOKEN_TITLE) {
-        return token
-      }
-    })
-
-    if (applicableAccessToken) {
-      return applicableAccessToken.access_token
-    }
-
-    const createdAccessToken = await this.createStorefrontAccessToken()
-
-    return createdAccessToken.access_token
-  }
 }
 
 /**
